5665926596757e0505dcb4ba321b7978333c476a
Christopher Dunne <christopher.dunne65@mail.dcu.ie>
2019-03-03 17:26:54 +0000

    Added source code.

---
+++ final-year-project /Database/DBConfig.php
@@ -0,0 +1,16 @@
+<?php
+
+//Define your host here.
+$HostName = "localhost";
+
+//Define your database name here.
+//Define your database name here.
+$DatabaseName = "users";
+
+//Define your database username here.
+$HostUser = "root";
+
+//Define your database password here.
+$HostPass = "password";
+
+?>
---
+++ final-year-project /eventsrepublic/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Root, Tabs } from './src/config/router';
+
+class App extends Component {
+    render() {
+        return <Root />;
+    }
+}
+
+export default App;
+
+// import Settings from '../eventsrepublic/src/components/Settings'
+//
+//
+//
+//
+//
+//
+// type Props = {};
+// export default class App extends Component<Props> {
+//     render() {
+//         return (
+//             <Settings></Settings>
+//
+//         );
+//     }
+// }
